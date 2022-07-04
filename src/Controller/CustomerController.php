21fbbc664b73010c41024855d3137017667fd5e9
d4rkstrife <p.gdc85@gmail.com>
2022-07-04 05:36:43 +0200

    finished signup and addCustomer routes

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -27,7 +28,17 @@ class CustomerController extends AbstractController
     #[Route('/api/customers/', name: 'addCustomer', methods: ['POST'])]
     public function addOne(CustomerRepository $customerRepo, Request $request, SerializerInterface $serializer, ValidatorInterface $validator): Response
     {
-        //tester request getcontent
+        if (!$request->getContent()) {
+            return new Response('
+            Le formulaire doit être présenté comme suit:
+            {
+                "firstName":"",
+                "lastName":"",
+                "adress":"",
+                "email":""
+            }
+            ', 400);
+        }
         $customer = $serializer->deserialize($request->getContent(), Customer::class, 'json');
         if ($customer->getAdress() === null || $customer->getFirstName() === null || $customer->getLastName() === null || $customer->getEmail() === null) {
             //dd('ce champ doit etre rempli');
@@ -39,7 +50,7 @@ class CustomerController extends AbstractController
                 "adress":"",
                 "email":""
             }
-            ');
+            ', 400);
         }
 
         $customer->setCreatedAt(new DateTime());
@@ -56,10 +67,10 @@ class CustomerController extends AbstractController
         }
         $customerRepo->add($customer);
 
-        return $this->json('ok');
+        return $this->json($customer, 201, context: ['groups' => 'customer:read']);
     }
 
-    #[Route('/api/customers/{uuid}', name: 'customer_details', methods: ['GET'])]
+    #[Route('/api/customers/{customer_uuid}', name: 'customer_details', methods: ['GET'])]
     public function productDetails(Customer $customer): Response
     {
         return $this->json($customer, 201, context: ['groups' => 'customer:read']);
